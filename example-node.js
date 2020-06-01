4b14a0f41e3e3c04a0dcf5742a22a2b2d64cbef8
dthuylinhh1 <54965669+dthuylinhh1@users.noreply.github.com>
2020-06-01 09:30:34 +0700

    adding a proper start script

--- example-node.js
+++ example-node.js
@@ -1,1 +1,23 @@
 const express = require('express');
+const app = express();
+const https = require('https');
+
+app.get('/', (request, response)=> {
+    https.get('https://insult.mattbas.org/api/insult', resp => {
+        resp.on('data', chunk =>{
+            response.send(`
+                <http>
+                    <head><title> Oh Snap </title></head>
+                    <body>
+                        <h1> OH SNAP </h1>
+                        <p style="font-size: 24px; color:red;">
+                            ${chunk.toString()}!
+                        </p>
+                    </body>
+                </http>
+            `);
+        });
+    });
+});
+
+app.listen(process.env.PORT || 3000);
